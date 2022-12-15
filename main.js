ea74e348497988fd74c9f947611bf3ea172841ce
albertoBarl <alberto.barletta00@gmail.com>
2022-12-15 16:34:29 +0100

    added .container content from js

--- main.js
+++ main.js
@@ -60,3 +60,50 @@ const posts = [
     created: "2021-03-05",
   },
 ];
+
+let content = "";
+posts.forEach((elements) => {
+  content += `<div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img
+            class="profile-pic"
+            src="${elements.author.image}"
+            alt="IMG not available"
+          />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${elements.author.name}</div>
+          <div class="post-meta__time">${elements.created}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+    ${elements.content}
+    </div>
+    <div class="post__image">
+      <img src="${elements.media}" alt="IMG not available" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" href="#" data-postid="1">
+            <i
+              class="like-button__icon fas fa-thumbs-up"
+              aria-hidden="true"
+            ></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
+        </div>
+        <div class="likes__counter">
+          Piace a
+          <b id="like-counter-1" class="js-likes-counter">${elements.likes}</b> persone
+        </div>
+      </div>
+    </div>
+  </div>`;
+});
+
+let container = document.getElementById("container");
+container.innerHTML += content;
